1ab9e9eae6e684b5f964c368a96898b1777098a5
Mikhail Mefodyev <hg-4ybak@mail.ru>
2024-05-26 21:09:22 +0300

    feat: add reports from first stage of first experiment

---
+++ lab5/watcher.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+log_path="$1"
+target_process="$2"
+
+> "$log_path"
+
+system_memory() {
+	top -bn1 | grep "MiB"
+}
+
+mem_script() {
+	top -bn1 | grep "$target_process"
+}
+
+top_five() {
+	top -bn1 | tail -n +8 | head -n 5 | awk '{ print $12 }'
+}
+
+analyze() {
+	{
+		echo "==================="
+		echo "$( system_memory )"
+		echo ""
+		echo "$( mem_script )"
+		echo ""
+		echo "$( top_five )"
+		echo "==================="
+		echo ""
+	} >> "$log_path"
+}
+
+while true; do
+	analyze
+	sleep 1s
+done
